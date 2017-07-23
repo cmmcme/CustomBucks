44069e8ddb462a8e3183f0f3d39c5c3a9b0dedf2
cmmcme <cmmcme0604@gmail.com>
2017-07-23 18:35:17 +0900

    route the menu

--- routes/menu.js
+++ routes/menu.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-var viewPath = 'partials/menu/';
+var viewPath = 'partials/kinds/';
 
-router.get('/view', function(req, res, next) {
-    res.render(viewPath + 'view');
+router.get('/', function(req, res, next) {
+
+    // TODO::디비에서 데이터를 가져오는걸로 바꾸어야함
+    let kinds = [
+        {
+            title: '콜드 브루'
+        },
+        {
+            title: '프라푸치노'
+        }
+    ];
+    res.render(viewPath + 'view', {
+        kinds: kinds
+    });
 });
 
-router.get('/new', function(req, res, next) {
-    res.render(viewPath + 'new');
+router.get('/:kind/menu', function(req, res, next) {
+    // TODO::디비에서 데이터를 가져오는걸로 바꾸어야함
+   let menus = [
+       {
+           title: '아메리카노'
+       },
+       {
+           title: '에스프레소'
+       }
+   ];
+   res.render(viewPath + 'menu/view', {
+       menus: menus
+   });
 });
 
 module.exports = router;
